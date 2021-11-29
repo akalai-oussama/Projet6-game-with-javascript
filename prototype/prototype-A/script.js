e811ba77c210fd98aeff5f6d2fff2c2b7f4fba52
oussamaa koz <oussamaakalay76@gmail.com>
2021-11-29 04:23:17 -0800

    add

---
+++ prototype/prototype-A/script.js
@@ -0,0 +1,23 @@
+var randomNumber = Math.floor(Math.random() * 100);
+
+var userInput = prompt("deviner en un nombre entre 1 et 100:");
+var winMessage = "Félicitations, vous avez gagné après " ;
+var lostMessage = "C'est raté !!";
+
+var itIsSmall = "C'est plus petit que " + userInput + ""
+var itIsBigger = "C'est plus grand que " + userInput + ""
+var reuslt;
+
+function isCorrect() {
+        if (userInput == randomNumber){
+            reuslt = winMessage;
+        } else if (userInput > randomNumber){
+            reuslt = itIsSmall
+        } else if (userInput < randomNumber){
+            reuslt = itIsBigger
+        }
+
+        alert(reuslt)
+}
+
+isCorrect() 
